dfb2e360632d6127a28a45946acb3ad1654aa7ab
Yogesh Yadav <63114831+yogesh1331@users.noreply.github.com>
2020-05-06 20:23:17 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='mypackage_name',
+version='0.1',
+description='Testing installation of Package',
+url='#',
+author='yy',
+author_email='test@gmail.com',
+license='MIT',
+packages=['mypackage'],
+zip_safe=False)
